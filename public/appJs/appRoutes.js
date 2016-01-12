5ad925ed66f91ff7092512ed6cb6159e597fa67b
Chetan naik <chetan@scriptlanes.com>
2016-01-12 16:27:22 +0530

    minor change

--- public/appJs/appRoutes.js
+++ public/appJs/appRoutes.js
@@ -687,7 +687,7 @@ app.config(function ($routeProvider, $locationProvider) {
                         var title = data.meta_data_meta_title;
 
                         //We trigger a new window with the Twitter dialog, in the middle of the page
-                        window.open('http://twitter.com/share?url=' + loc + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + $(window).width() / 2 + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+                        window.open('http://twitter.com/share?url=' + loc + '&text=' + title, 'twitterwindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + $(window).width() / 2 + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
 
                     });
 
